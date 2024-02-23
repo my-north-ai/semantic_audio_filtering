51ffe451ef9597cd027e94aa0889a265f30b83eb
AIdevMyNorth <yperezhohin@mynorth.ai>
2024-02-23 13:36:47 +0000

    Added Hf token to settings, created a dataset to follow the structure of data augmentation and pushed it to the hub

--- tts_data_augmentation/configurations/config.py
+++ tts_data_augmentation/configurations/config.py
@@ -3,4 +3,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     OPENAI_KEY: str
+    HF_TOKEN: str
     model_config = SettingsConfigDict(env_file=".env")
